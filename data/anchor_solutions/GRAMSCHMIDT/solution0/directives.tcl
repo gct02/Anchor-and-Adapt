1acf47f6c6fa211e019440afec846ced90b694be
Gabriel Tavares <gabrielc.tavares02@gmail.com>
2025-09-14 17:00:42 -0300

    Included anchor solutions

---
+++ data/anchor_solutions/GRAMSCHMIDT/solution0/directives.tcl
@@ -0,0 +1,12 @@
+set_directive_pipeline -off kernel_gramschmidt/loop1
+set_directive_pipeline -off kernel_gramschmidt/loop2
+set_directive_pipeline -off kernel_gramschmidt/loop3
+set_directive_pipeline -off kernel_gramschmidt/loop4
+set_directive_pipeline -off kernel_gramschmidt/loop5
+set_directive_pipeline -off kernel_gramschmidt/loop6
+set_directive_pipeline -off kernel_gramschmidt
+set_directive_loop_flatten -off kernel_gramschmidt/loop2
+set_directive_loop_flatten -off kernel_gramschmidt/loop3
+set_directive_loop_flatten -off kernel_gramschmidt/loop4
+set_directive_loop_flatten -off kernel_gramschmidt/loop5
+set_directive_loop_flatten -off kernel_gramschmidt/loop6
